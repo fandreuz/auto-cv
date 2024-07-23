628ac85e58647eae48e3a26f5d984d842450d774
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-23 21:57:15 +0200

    cleanup and zip

--- cv-rendering-lambda/bootstrap-environment.sh
+++ cv-rendering-lambda/bootstrap-environment.sh
@@ -8,5 +8,9 @@
 set -eux
 
 rm -rf $target_dir
+rm -rf $target_dir.zip
 mkdir $target_dir
 $python_executable -m pip install $source_code_dir --target $target_dir
+
+zip -r $target_dir.zip $target_dir
+rm -rf $target_dir
