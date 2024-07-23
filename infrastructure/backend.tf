88f5f6248e0b10cfc3f3d7f5356bf6b7cb51d104
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-24 01:45:34 +0200

    Revert "tmp"
    This reverts commit 93a4ddeec61734ab7950a4e285611a0365850e14.

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -25,3 +25,17 @@ resource "aws_iam_role" "iam_cv_rendering_lambda" {
   name               = "iam_cv_rendering_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+resource "aws_lambda_function" "test_lambda" {
+  function_name = "cv-rendering"
+  description   = "Render CV"
+  handler       = "cv_rendering.lambda.lambda_handler"
+  runtime       = "python3.11"
+
+  role        = aws_iam_role.iam_cv_rendering_lambda.arn
+  memory_size = 128
+  timeout     = 300
+
+  s3_bucket = var.s3_bucket_id
+  s3_key    = var.s3_bucket_key
+}
