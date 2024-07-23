85472faf725ad186a61d8baec47dcdf8d9e83d06
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-23 20:20:53 +0200

    main

---
+++ infrastructure/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  cloud {
+    organization = "auto-cv"
+    workspaces {
+      name = "auto-cv-pro"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.59.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.2"
+    }
+  }
+}
