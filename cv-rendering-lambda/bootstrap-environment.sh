2ee00085e5d9253bc716213652ad01d39822c117
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-24 00:41:04 +0200

    better packaging

--- cv-rendering-lambda/bootstrap-environment.sh
+++ cv-rendering-lambda/bootstrap-environment.sh
@@ -12,7 +12,10 @@ set -eux
 rm -rf $target_dir
 rm -rf $target_dir.zip
 mkdir $target_dir
+
 $python_executable -m pip install $source_code_dir --target $target_dir
+find target -type d -name '*.dist-info' -exec rm -rf {} +
+find target -type d -name '__pycache__' -exec rm -rf {} +
 
 zip -r $target_dir.zip $target_dir
 rm -rf $target_dir
