7f4156406fd7b1aff662e06531cee4d93b1db7cd
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-24 18:09:38 +0200

    layers

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -26,16 +26,34 @@ resource "aws_iam_role" "iam_cv_rendering_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+resource "aws_lambda_layer_version" "lambda_dependencies_pkg" {
+  layer_name = "lambda_dependencies_pkg_layer"
+  s3_bucket  = "cv-rendering-pkg-bucket20240723210849196900000001"
+  s3_key     = "cv_rendering_dependencies.zip"
+
+  compatible_runtimes = ["python3.11"]
+}
+
+resource "aws_lambda_layer_version" "lambda_code_pkg" {
+  layer_name = "lambda_code_pkg_layer"
+  s3_bucket  = "cv-rendering-pkg-bucket20240723210849196900000001"
+  s3_key     = "cv_rendering_dependencies.zip"
+
+  compatible_runtimes = ["python3.11"]
+}
+
 resource "aws_lambda_function" "test_lambda" {
   function_name = "cv-rendering"
   description   = "Render CV"
-  handler       = "cv_rendering.lambda.lambda_handler"
+  handler       = "lambda.lambda_handler"
   runtime       = "python3.11"
 
   role        = aws_iam_role.iam_cv_rendering_lambda.arn
   memory_size = 128
   timeout     = 300
 
-  s3_bucket = var.s3_bucket_id
-  s3_key    = var.s3_bucket_key
+  layers = [
+    aws_lambda_layer_version.lambda_dependencies_pkg.arn,
+    aws_lambda_layer_version.lambda_code_pkg.arn
+  ]
 }
