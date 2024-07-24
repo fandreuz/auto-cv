755b6cee3511dfbf7015f861d9a12a83cccd22c2
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-25 01:56:32 +0200

    add permissions to logs for lambda

--- infrastructure/lambda_iam.tf
+++ infrastructure/lambda_iam.tf
@@ -13,3 +13,28 @@ resource "aws_iam_role" "render_lambda_execution_role" {
   name_prefix        = "render_lambda_execution_role"
   assume_role_policy = data.aws_iam_policy_document.render_lambda_execution_role.json
 }
+
+data "aws_iam_policy_document" "render_lambda_logs_policy" {
+  statement {
+    resources = "*"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+      "logs:GetLogEvents",
+      "logs:FilterLogEvents"
+    ]
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "render_lambda_logs_policy" {
+  policy = data.aws_iam_policy_document.render_lambda_logs_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "render_lambda_logs_policy_attachment" {
+  role       = aws_iam_role.render_lambda_execution_role.name
+  policy_arn = aws_iam_policy.render_lambda_logs_policy.arn
+}
