079168fa6e8d027e9a5d69972e61bf7bdf5065e4
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-25 00:16:14 +0200

    sqs policy

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -29,3 +29,77 @@ resource "aws_lambda_event_source_mapping" "work_queue_to_lambda" {
   function_name                      = aws_lambda_function.rendering_lambda.arn
   maximum_batching_window_in_seconds = 10
 }
+
+# https://gist.github.com/afloesch/dc7d8865eeb91100648330a46967be25
+resource "aws_iam_role" "assume_role" {
+  name_prefix = "auto-cv-assume-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "queue_writer_policy" {
+  name_prefix = "auto-cv-work-queue-writer"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents",
+          "logs:GetLogEvents",
+          "logs:FilterLogEvents"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "sqs:GetQueueUrl",
+          "sqs:ChangeMessageVisibility",
+          "sqs:ListDeadLetterSourceQueues",
+          "sqs:SendMessageBatch",
+          "sqs:PurgeQueue",
+          "sqs:ReceiveMessage",
+          "sqs:SendMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:CreateQueue",
+          "sqs:ListQueueTags",
+          "sqs:ChangeMessageVisibilityBatch",
+          "sqs:SetQueueAttributes"
+        ],
+        "Resource": "${aws_sqs_queue.work_queue.arn}"
+      },
+      {
+        "Effect": "Allow",
+        "Action": "sqs:ListQueues",
+        "Resource": "*"
+      }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "api" {
+  role       = aws_iam_role.assume_role.name
+  policy_arn = aws_iam_policy.queue_writer_policy.arn
+}
