80811798e35bca0ba608f4bbe1e4c5fde5e6ada4
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-24 19:05:56 +0200

    Revert "tmp"
    This reverts commit 056c15a8bd4fb7dbfbf90946b033b752a0f83174.

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -20,3 +20,31 @@ resource "aws_iam_role" "iam_cv_rendering_lambda" {
   name               = "iam_cv_rendering_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+resource "aws_lambda_layer_version" "lambda_dependencies_pkg" {
+  layer_name = "lambda_dependencies_pkg_layer"
+  s3_bucket  = "cv-rendering-pkg-bucket20240723210849196900000001"
+  s3_key     = "cv_rendering_dependencies.zip"
+
+  compatible_runtimes = ["python3.11"]
+}
+
+data "archive_file" "lambda_code_pkg" {
+  type        = "zip"
+  source_file = "${path.module}/../cv-rendering-lambda/lambda.py"
+  output_path = "lambda.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  function_name = "cv-rendering"
+  description   = "Render CV"
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.11"
+
+  role        = aws_iam_role.iam_cv_rendering_lambda.arn
+  memory_size = 128
+  timeout     = 300
+
+  filename = data.archive_file.lambda_code_pkg.output_path
+  layers   = [aws_lambda_layer_version.lambda_dependencies_pkg.arn]
+}
