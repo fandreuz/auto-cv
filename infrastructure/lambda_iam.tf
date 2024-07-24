e6e0045dd4ceb6ebb041d59504c3347e5558dca8
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-25 01:17:55 +0200

    cleanup lambda exec roles

--- infrastructure/lambda_iam.tf
+++ infrastructure/lambda_iam.tf
@@ -1,17 +1,58 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+resource "aws_iam_role" "render_lambda_execution_role" {
+  name_prefix = "render_lambda_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Resource = "*"
+        Action   = ["sts:AssumeRole"]
+        Effect   = "Allow"
+      },
+    ]
+  })
 }
 
-resource "aws_iam_role" "render_lambda_execution_role" {
-  name_prefix        = "render_lambda_execution_role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+resource "aws_iam_role_policy_attachment" "example_lambda" {
+  role       = aws_iam_role.render_lambda_execution_role.name
+  policy_arn = aws_iam_policy.render_lambda_execution_policy.arn
+}
+
+resource "aws_iam_policy" "render_lambda_execution_policy" {
+  policy = jsonencode({
+    Statement = [{
+      Sid       = "AllowSQSPermissions"
+      Effect    = "Allow"
+      Resources = ["arn:aws:sqs:*"]
+      Actions = [
+        "sqs:ChangeMessageVisibility",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:ReceiveMessage",
+      ] }
+    ]
+
+    Statement = [{
+      Sid       = "AllowInvokingLambdas"
+      Effect    = "Allow"
+      Resources = ["arn:aws:lambda:ap-southeast-1:*:function:*"]
+      Actions   = ["lambda:InvokeFunction"]
+    }]
+
+    Statement = [{
+      Sid       = "AllowCreatingLogGroups"
+      Effect    = "Allow"
+      Resources = ["arn:aws:logs:ap-southeast-1:*:*"]
+      Actions   = ["logs:CreateLogGroup"]
+    }]
+
+    Statement = [{
+      Sid       = "AllowWritingLogs"
+      Effect    = "Allow"
+      Resources = ["arn:aws:logs:ap-southeast-1:*:log-group:/aws/lambda/*:*"]
+      Actions = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+      ]
+    }]
+  })
 }
