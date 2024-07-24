6237b8eb2f3229045a6ce6eecf2d4a00b21e991b
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-24 18:52:47 +0200

    nn

--- cv-rendering-lambda/bucket.tf
+++ cv-rendering-lambda/bucket.tf
@@ -16,11 +16,6 @@ variable "dependencies_pkg_filename" {
   default = "cv_rendering_dependencies.zip"
 }
 
-variable "lambda_code_pkg_filename" {
-  type    = string
-  default = "cv_rendering_lambda.zip"
-}
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -45,22 +40,10 @@ resource "aws_s3_object" "dependencies_pkg" {
   source_hash = filemd5("${path.module}/${var.dependencies_pkg_filename}")
 }
 
-resource "aws_s3_object" "lambda_code_pkg" {
-  bucket      = aws_s3_bucket.cv_rendering_pkg_bucket.id
-  key         = var.lambda_code_pkg_filename
-  source      = "${path.module}/${var.lambda_code_pkg_filename}"
-  etag        = filemd5("${path.module}/${var.lambda_code_pkg_filename}")
-  source_hash = filemd5("${path.module}/${var.lambda_code_pkg_filename}")
-}
-
 output "s3_bucket_id" {
   value = aws_s3_bucket.cv_rendering_pkg_bucket.id
 }
 
 output "s3_dependencies_bucket_key" {
   value = aws_s3_object.dependencies_pkg.key
-}
-
-output "s3_lambda_bucket_key" {
-  value = aws_s3_object.lambda_code_pkg.key
 }
