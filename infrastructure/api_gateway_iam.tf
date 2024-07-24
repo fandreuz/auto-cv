f985c7b884ae1a5d87ea4f7b7dd6e6846b27518d
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-25 01:23:59 +0200

    jsonify role

--- infrastructure/api_gateway_iam.tf
+++ infrastructure/api_gateway_iam.tf
@@ -1,19 +1,18 @@
 resource "aws_iam_role" "queue_writer_role" {
   name_prefix = "auto-cv-queue-writer-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Principals = {
+          type        = "Service"
+          Identifiers = ["apigateway.amazonaws.com"]
+        }
+        Action = ["sts:AssumeRole"]
+        Effect = "Allow"
+        "Sid" : ""
       },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
