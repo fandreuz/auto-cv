f5c1fe61f32e04ab25703b4a93e24fcfd46458d9
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-24 19:45:33 +0200

    fix path parts

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -3,23 +3,29 @@ resource "aws_api_gateway_rest_api" "gateway" {
   description = "Terraform Serverless Application Example"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
-  path_part   = "api/v1/cv"
+  path_part   = "v1"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_resource" "cv_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  parent_id   = "${aws_api_gateway_resource.api_resource.id}"
+  path_part   = "cv"
+}
+
+resource "aws_api_gateway_method" "cv_resource" {
   rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  resource_id   = "${aws_api_gateway_resource.cv_resource.id}"
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "gateway_lambda_integration" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  resource_id = "${aws_api_gateway_method.cv_resource.resource_id}"
+  http_method = "${aws_api_gateway_method.cv_resource.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -34,5 +40,5 @@ resource "aws_api_gateway_deployment" "gateway" {
 }
 
 output "lambda_url" {
-  value = "${aws_api_gateway_deployment.gateway.invoke_url}/${aws_api_gateway_resource.proxy.path_part}"
+  value = "${aws_api_gateway_deployment.gateway.invoke_url}/${aws_api_gateway_resource.api_resource.path_part}/${aws_api_gateway_resource.proxy.path_part}"
 }
