550a07486575588498fe116c6344a5c4be18336b
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-07-24 19:49:18 +0200

    fixes

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -52,7 +52,7 @@ resource "aws_lambda_function" "rendering_lambda" {
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.rendering_lambda.function_name}"
+  function_name = aws_lambda_function.rendering_lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/*"
